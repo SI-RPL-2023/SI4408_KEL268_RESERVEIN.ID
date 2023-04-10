11a58fe0d384ade852a4dcca079d07d1c4661969
ivanmr0 <ivanmaulanarazzaq15@gmail.com>
2023-04-10 22:06:12 +0700

    Update Landing page ref #8

--- resources/views/profile/partials/navigation.blade.php
+++
@@ -1,7 +0,0 @@
-<section>
-    <div class="flex -mb-4 gap-10 text-lg mt-5">
-        <a href="/profile"><div class="">Account</div></a>
-        <a href="/profile/loyalty"><div class="">Loyalty</div></a>
-        <a href="/profile/password"><div class="">Account Password</div></a>
-    </div>
-</section>
